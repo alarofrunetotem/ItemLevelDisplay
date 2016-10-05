7fa4667e445348b7bc320312657d44d43b1f7726
ggargani <giovanni@gargani.it>
2016-10-05 21:34:01 +0200

    Blizzard bags

--- ILD-Blizzard/core.lua
+++ ILD-Blizzard/core.lua
@@ -4,3 +4,8 @@ local module=ns.module
 function module:IsItemButton(name)
 	return name:find("ContainerFrame%dItem")
 end
+function module:OnInitialized()
+	for i=1,5 do
+		self:SetTriggerFrame(("ContainerFrame%d"):format(i))
+	end
+end
