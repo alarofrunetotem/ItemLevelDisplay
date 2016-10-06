d54bc8bc261d361bfc3c9c82e33f4d08fb95b8f1
ggargani <giovanni@gargani.it>
2016-10-06 18:04:39 +0200

    More hooks added for bag modules

--- Bags.lua
+++ Bags.lua
@@ -22,11 +22,12 @@ if LibDebug then LibDebug() ns.print=print else ns.print=function() end end
 --[===[@non-debug@
 ns.print=function() end
 --@end-non-debug@]===]
-local print=ns.print or print
+--local print=ns.print or print
 print("Bags loaded from",me)
 LoadAddOn("ItemLevelDisplay")
 local addon=LibStub("AceAddon-3.0"):GetAddon("ItemLevelDisplay")
-local module=LibStub("LibInit"):NewAddon(ns,me,{noswitch=true,profile=false,enhancedProfile=false,nogui=true,nohelp=true},'AceHook-3.0','AceEvent-3.0','AceTimer-3.0') --#Module
+--local module=LibStub("LibInit"):NewAddon(ns,me,{noswitch=true,profile=false,enhancedProfile=false,nogui=true,nohelp=true},'AceHook-3.0','AceEvent-3.0','AceTimer-3.0') --#Module
+local module=LibStub("AceAddon-3.0"):NewAddon(ns,me,'AceConsole-3.0','AceHook-3.0','AceEvent-3.0','AceTimer-3.0') --#Module
 local I=LibStub("LibItemUpgradeInfo-1.0")
 --local module=addon:NewSubModule(me,"AceHook-3.0","AceEvent-3.0") --#module
 local toc=select(4,GetBuildInfo())
@@ -34,8 +35,9 @@ local C=addon:GetColorTable()
 local L=addon:GetLocale()
 local hookedFrames={}
 local triggerFrames={}
-function module:OnInitialized()
+function module:OnInitialize()
 	self:Print(me,"loaded")
+	self:OnInitialized()
 end
 function module:SetTriggerFrame(frame)
 	if type(frame)=="string" then
@@ -72,45 +74,60 @@ function bagManagers:baggins()
 	end
 	self:SetTriggerFrame("BagginsBag1")
 end
-
-function bagManagers:arkinventory()
-	local frame=ArkInventory.Frame_Main_Get(ArkInventory.Const.Location.Bag)
-	canDisplayLevel=function()
-		return not ArkInventory.Global.Location[ArkInventory.Const.Location.Bag].isOffline
-	end
-	findItemButtons=function()
-		self:scanForBagItemButtons('ARKINV')
-		findItemButtons=nop
-	end
-	self:SetTriggerFrame(frame)
-end
-function module:IsItemButton(name)
+function module:IsItemButton(name,frame)
 	return name:find("ContainerFrame%dItem")
 end
 function module:IsOfflineBag()
 	return false
 end
+-- Placeholder
+function module:ReHook(this)
+end
 function module:FirstBagDisplay(this)
 	for frame,_ in pairs(triggerFrames) do
 		self:Unhook(frame,"OnShow")
 	end
+	self:ReHook(this)
 --	print("BagOpen",this:GetName(),this.isOffLine)
 	self:RegisterEvent("BAG_UPDATE_DELAYED")
-	return self:scanForBagItemButtons()
+	return self:ScanForBagItemButtons(this)
 end
-function module:scanForBagItemButtons()
+function module:FirstBankDisplay(this)
+	self:UnregisterEvent("BANKFRAME_OPENED")
+	return self:ScanForBagItemButtons(this)
+end
+function module:GetItemInfo(frame)
+	local bag,slot=frame:GetParent():GetID(), frame:GetID()
+	return GetContainerItemInfo(bag,slot)
+end
+function module:HookFrame(hookedFrames,frame)
+	if not hookedFrames[frame] then
+		local rc,message=pcall(self.SecureHookScript,self,frame,"OnShow","OnBagItemShow")
+--@debug@
+		if not rc then
+			print(name.." : "..message)
+		end
+--@end-debug@
+		hookedFrames[frame]=addon:addLayer(frame,frame:GetName(),true)
+	end
+end
+function module:ScanForBagItemButtons(main)
 	local frame = EnumerateFrames()
 	local elapsed=GetTime()
 	local batch,batchSize=0,500
 	local s=0
+	local first=true
 	while frame do
 		local name=frame:GetName()
 		s=s+1
 		batch=batch+1
-		if name then
+		if frame:GetObjectType()=="Button" and name then
 			if  -- Using this set of feature as a signature for itemcontainer derived frames
-				self:IsItemButton(name)
-				and _G[name .."Cooldown"]
+				self:IsItemButton(name) then
+--@debug@
+--				print("Verifying",name,frame)
+--@end-debug@
+				if  _G[name .."Cooldown"]
 				and  _G[name .."IconQuestTexture"]
 				and frame.newitemglowAnim
 				and frame.flashAnim
@@ -119,26 +136,14 @@ function module:scanForBagItemButtons()
 				and frame.NewItemTexture
 				and frame.BattlepayItemTexture
 				then
-				local bag,slot=frame:GetParent():GetID(), frame:GetID()
-				if bag and slot then
-					if not hookedFrames[frame] then
-						local rc,message=pcall(self.SecureHookScript,self,frame,"OnShow","OnBagItemShow")
-						hookedFrames[frame]=addon:addLayer(frame,frame:GetName(),true)
-						if frame:IsVisible() then
-							self:OnBagItemShow(frame)
-						end
---@debug@
-						if not rc then
-							print(name.." : "..message)
-						end
---@end-debug@
-					end
+					if self:HookFrame(hookedFrames,frame) then break end -- Allow an early exit
 				end
 			end
 		end
 		if coroutine.running() and batch > batchSize then batch=0 coroutine.yield(true) end
 		frame = EnumerateFrames(frame)
 	end
+	self:ScheduleTimer("BagRefresh",0.2)
 --@debug@
 	local i=0
 	for _,_ in pairs(hookedFrames) do i=i+1 end
@@ -162,14 +167,17 @@ end
 function module:BAG_UPDATE_DELAYED()
 	self:BagRefresh()
 end
+
 function module:OnBagItemShow(this,...)
+	--@debug@
+	--print("OnSHow",this:GetName(),this:GetInfo())
+	--@end-debug@
 	if not this:IsVisible() then return end
 	if not hookedFrames[this] then return end
 	local t=hookedFrames[this].ilevel
 	if self:IsOfflineBag() then t:Hide() end
-	local bag,slot=this:GetParent():GetID(), this:GetID()
-	if bag and slot then
-		local itemrarity,_,_,itemlink=select(4,GetContainerItemInfo(bag,slot))
+	local quality,_,_,itemlink=select(4,self:GetItemInfo(this))
+	if quality then
 		if itemlink then
 			if hookedFrames[this].itemlink==itemlink then return end
 			hookedFrames[this].itemlink=itemlink
@@ -179,7 +187,7 @@ function module:OnBagItemShow(this,...)
 			--local font="NumberFont_Outline_Huge"
 			local ilevel=I:GetUpgradedItemLevel(itemlink)
 			t:SetFormattedText("%3d",ilevel)
-			t:SetTextColor(addon:getColors(itemrarity,ilevel))
+			t:SetTextColor(addon:getColors(quality,ilevel))
 			t:Show()
 			return true
 		else
