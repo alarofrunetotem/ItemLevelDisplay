1a87dc62b0787f164a94666dfe870f5f9970bed9
Alar of Runetotem <alar@aspide.it>
2016-10-27 10:07:56 +0200

    Ilevel for Blizzard Bags fixed

--- ILD-Blizzard/core.lua
+++ ILD-Blizzard/core.lua
@@ -3,32 +3,11 @@ local addon=ns.addon
 local module=ns.module
 local toc=select(4,GetBuildInfo())
 function module:OnEnabled()
-	self:SecureHook("BankFrameItemButton_Update","DisplayBank")
-	if toc > 70000 then
-		self:SecureHook("ContainerFrameItemButton_UpdateItemUpgradeIcon","Display")
-	else
-		self:SecureHook("ContainerFrame_Update","DisplayBag")
-	end
+	self:SecureHook("BankFrameItemButton_Update","Display")
+	self:SecureHook("ContainerFrameItemButton_UpdateItemUpgradeIcon","Display")
 
 end
-function module:Display(...)
-	print(...)
-
-end
-function module:DisplayBank(button,...)
-	if( button.isBag ) then
-		return
-	end
+function module:Display(button,...)
 	local quality,itemlink,class=self:GetInfo(nil,button:GetParent():GetID(),button:GetID())
 	return self:DrawItem(button,quality,itemlink,class)
-
-end
-function module:DisplayBag(frame,...)
-	local bagId = frame:GetID();
-	local name = frame:GetName();
-	for i=1,frame.size do
-		local button=_G[name .. "Item" ..i]
-		local quality,itemlink,class=self:GetInfo(nil,bagId,button:GetID())
-		self:DrawItem(button,quality,itemlink,class)
-	end
 end
--- RelNotes.lua
+++ RelNotes.lua
@@ -32,6 +32,9 @@ Bags management is implemented via separated addon!! (ILD-<bagmanager name>)
 * ILD-Onebag3: Onebag3 (with bank)
 
 ]])
+self:RelNotes(2,0,5,[[
+Fix: ItemLevel in Blizzard bags is now back
+]])
 self:RelNotes(2,0,4,[[
 Toc: 7.1.0 compatible
 ]])
