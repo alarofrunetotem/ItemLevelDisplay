7dc3d89d07209731eac10c1eac71ef259bf57dbe
Giovanni G <19211824+alar77@users.noreply.github.com>
2020-10-20 09:11:41 +0200

    Updated MSADropDown

--- RelNotes.lua
+++ RelNotes.lua
@@ -32,7 +32,9 @@ Bags management is implemented via separated addon!! (ILD-<bagmanager name>)
 * ILD-Onebag3: Onebag3 (with bank)
 
 ]])
-self:RelNotes(2,2,2,[[
+self:RelNotes(2,2,3,[[
+Fix: Repackaged to pull the right MSADropdown version
+]])self:RelNotes(2,2,2,[[
 Fix: LiteBag works again
 ]])
 self:RelNotes(2,2,1,[[
